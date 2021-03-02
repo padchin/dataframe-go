01470129850b6678c6e3335672c4c2919495be3a
padchin <werdna805@gmail.com>
2021-03-02 17:49:30 +0300

    Реорганизация расположения функций

--- plot/plot.go
+++ plot/plot.go
@@ -10,11 +10,15 @@ package plot
 
 import (
 	"bytes"
+	"context"
 	"encoding/base64"
 	"errors"
+	"github.com/padchin/dataframe-go"
+	"github.com/wcharczuk/go-chart/v2"
 	"net/url"
 	"strings"
 	"text/template"
+	"time"
 
 	"github.com/zserge/lorca"
 )
@@ -241,4 +245,128 @@ func (p *Plot) Display(scrollbar Scrollbar, mime ...MIME) error {
 	}
 
 	return p.ui.Load("data:text/html," + url.PathEscape(builder.String()))
+}
+
+// S converts a SeriesFloat64 to a chart.Series for use with the "github.com/wcharczuk/go-chart" package.
+// Currently x can be nil, a SeriesFloat64 or a SeriesTime. nil values in the x and y Series are ignored.
+//
+// NOTE: To "unjoin" the lines, you can adjust the style to chart.Style{StrokeWidth: chart.Disabled, DotWidth: 2}.
+func S(ctx context.Context, y *dataframe.SeriesFloat64, x interface{}, r *dataframe.Range, style ...chart.Style) (chart.Series, error) {
+
+	var out chart.Series
+
+	if r == nil {
+		r = &dataframe.Range{}
+	}
+
+	yNRows := y.NRows(dataframe.DontLock)
+
+	start, end, err := r.Limits(yNRows)
+	if err != nil {
+		return nil, err
+	}
+
+	switch xx := x.(type) {
+	case nil:
+		cs := chart.ContinuousSeries{Name: y.Name(dataframe.DontLock)}
+
+		if len(style) > 0 {
+			cs.Style = style[0]
+		}
+
+		xVals := []float64{}
+		yVals := []float64{}
+
+		// Remove nil values
+		for i, j := 0, start; j < end+1; i, j = i+1, j+1 {
+
+			if err := ctx.Err(); err != nil {
+				return nil, err
+			}
+
+			yval := y.Values[j]
+
+			if dataframe.IsValidFloat64(yval) {
+				yVals = append(yVals, yval)
+				xVals = append(xVals, float64(i))
+			}
+		}
+
+		cs.XValues = xVals
+		cs.YValues = yVals
+
+		out = cs
+	case *dataframe.SeriesFloat64:
+
+		cs := chart.ContinuousSeries{Name: y.Name(dataframe.DontLock)}
+
+		if len(style) > 0 {
+			cs.Style = style[0]
+		}
+
+		xVals := []float64{}
+		yVals := []float64{}
+
+		// Remove nil values
+		for j := start; j < end+1; j++ {
+
+			if err := ctx.Err(); err != nil {
+				return nil, err
+			}
+
+			yval := y.Values[j]
+			xval := xx.Values[j]
+
+			if dataframe.IsValidFloat64(yval) {
+				// Check x val is valid
+				if dataframe.IsValidFloat64(xval) {
+					yVals = append(yVals, yval)
+					xVals = append(xVals, xval)
+				}
+			}
+		}
+
+		cs.XValues = xVals
+		cs.YValues = yVals
+
+		out = cs
+	case *dataframe.SeriesTime:
+
+		cs := chart.TimeSeries{Name: y.Name(dataframe.DontLock)}
+
+		if len(style) > 0 {
+			cs.Style = style[0]
+		}
+
+		xVals := []time.Time{}
+		yVals := []float64{}
+
+		// Remove nil values
+		for j := start; j < end+1; j++ {
+
+			if err := ctx.Err(); err != nil {
+				return nil, err
+			}
+
+			yval := y.Values[j]
+			xval := xx.Values[j]
+
+			if dataframe.IsValidFloat64(yval) {
+				// Check x val is valid
+				if xval != nil {
+					yVals = append(yVals, yval)
+					xVals = append(xVals, *xval)
+				}
+			}
+		}
+
+		cs.XValues = xVals
+		cs.YValues = yVals
+
+		out = cs
+	default:
+		panic("unrecognized x")
+	}
+
+	return out, nil
 }
